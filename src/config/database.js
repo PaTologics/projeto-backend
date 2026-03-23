14821081a18fd8f5a35f0e1a33194d0bac5bb1f8
Adrian <lllucky910@gmail.com>
2026-03-23 18:28:18 -0300

    refactor: reorganiza estrutura de testes e atualiza configs de banco

--- src/config/database.js
+++ src/config/database.js
@@ -3,10 +3,19 @@ require('dotenv').config();
 module.exports = {
   dialect: process.env.DB_DIALECT || 'postgres',
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 6543, // Adicionamos a porta aqui!
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   
+  // ESSA PARTE É OBRIGATÓRIA PARA SUPABASE
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Permite a conexão mesmo com certificados autoassinados
+    }
+  },
+
   logging: false, 
   
   define: {
